042964574c1d45d926bac38c3bddc6c9287e7df6
jascha <jascha@jaeh.at>
2020-04-08 19:42:56 +0200

    tests: update spec

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,9 +1,17 @@
 import { is } from '@magic/test'
 
-import defaultRunCluster, { run } from '../src/index.mjs'
+import defaultRunCluster, { run } from '../src/server.mjs'
+
+import handler from '../src/handler.mjs'
+
+import hosts from '../src/index.mjs'
 
 export default [
   { fn: () => run, expect: is.fn, info: 'runCluster is a function' },
   { fn: () => defaultRunCluster, expect: is.fn, info: 'runCluster default export is a function' },
   { fn: is.deep.eq(run, defaultRunCluster), info: 'runCluster exports are equal' },
+
+  { fn: is.fn(handler), info: 'handler is a function' },
+
+  { fn: is.object(hosts), info: 'index exports hosts object' },
 ]
