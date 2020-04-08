6ffb6c72de4ee95be9d06371885340f7a7a45fba
jascha <jascha@jaeh.at>
2020-04-08 19:47:14 +0200

    handler: log requests

--- src/handler.mjs
+++ src/handler.mjs
@@ -1,7 +1,11 @@
-import { lib } from '@grundstein/commons'
+import { lib, log } from '@grundstein/commons'
 
 export const handler = string => (req, res) => {
+  const startTime = log.hrtime()
+
   lib.respond(res, { code: 200, body: string })
+
+  log.timeTaken(startTime, 'hosts file response took')
 }
 
 export default handler
