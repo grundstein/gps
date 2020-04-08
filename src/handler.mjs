d58a4f284f86cc37317e78342b76f3ef1510b10a
jascha <jascha@jaeh.at>
2020-04-08 19:31:53 +0200

    handler: use @grundstein/commons.lib.respond

--- src/handler.mjs
+++ src/handler.mjs
@@ -1,35 +1,8 @@
-import URL from 'url'
+import { lib } from '@grundstein/commons'
 
-import mimes from '@magic/mime-types'
-import log from '@magic/log'
-import is from '@magic/types'
 
-import { formatLog, getFileEncoding, getRandomId, respond, sendFile } from './lib/index.mjs'
-
-export const handler = store => async (req, res) => {
-  // assign random id to make this call traceable in logs.
-  req.id = await getRandomId()
-
-  req.headers['x-forwarded-for'] = req.id
-
-  const startTime = log.hrtime()
-
-  let { url } = req
-  if (url.endsWith('/')) {
-    url = `${url}index.html`
-  }
-
-  if (store) {
-    const file = store.get(url)
-
-    if (file) {
-      sendFile(req, res, file)
-      formatLog(req, res, startTime, 'static')
-      return
-    }
-  }
-
-  respond(res, { body: '404 - not found.', code: 404 })
-
-  formatLog(req, res, startTime, 404)
+export const handler = string => (req, res) => {
+  lib.respond(res, { code: 200, body: string })
 }
+
+export default handler
