b79ed39ed0cdf3c84dbb0d8b7b58a1820c03cb8f
jascha <jascha@jaeh.at>
2020-04-11 20:56:07 +0200

    update tests

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,13 +1,13 @@
 import { is } from '@magic/test'
 
-import defaultRunCluster, { run } from '../src/index.mjs'
+import defaultGps, { gps } from '../src/index.mjs'
 
 import handler from '../src/handler.mjs'
 
 export default [
-  { fn: () => run, expect: is.fn, info: 'runCluster is a function' },
-  { fn: () => defaultRunCluster, expect: is.fn, info: 'runCluster default export is a function' },
-  { fn: is.deep.eq(run, defaultRunCluster), info: 'runCluster exports are equal' },
+  { fn: () => gps, expect: is.fn, info: 'gps is a function' },
+  { fn: () => defaultGps, expect: is.fn, info: 'gps default export is a function' },
+  { fn: is.deep.eq(gps, defaultGps), info: 'defaultGps and gps exports are equal' },
 
   { fn: is.fn(handler), info: 'handler is a function' },
 ]
