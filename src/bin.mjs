1b15d792dcf50dbcbd4ffd8d8e6781f84de2e119
jascha <jascha@jaeh.at>
2020-04-28 19:19:06 +0200

    src/bin.mjs: add --proxy-host and --proxy-port

--- src/bin.mjs
+++ src/bin.mjs
@@ -9,18 +9,24 @@ const opts = {
     ['--help', '-help', 'help', '--h', '-h'],
     ['--host', '-h'],
     ['--port', '-p'],
+    ['--proxy-host'],
+    ['--proxy-port'],
   ],
   default: {
     '--host': '0.0.0.0',
     '--port': 4343,
+    '--proxy-host': 'localhost',
+    '--proxy-port': 2350,
   },
   single: ['--host', '--port'],
   help: {
     name: 'gps: grundstein proxy/positioning server',
     header: 'load balancer. public app entry point. single point of failure.',
     options: {
-      '--host': 'internal hostname to listen to, default 0.0.0.0',
-      '--port': 'port, default 4343',
+      '--host': 'hostname to listen to',
+      '--port': 'port to listen to',
+      '--proxy-host': 'host the proxied server listens to',
+      '--proxy-port': 'port the proxied server listens to',
     },
     example: `
 # run proxy and positioning services:
