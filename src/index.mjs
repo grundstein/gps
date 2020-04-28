da1b82807b15dec0bec16e461b08c779eca96259
jascha <jascha@jaeh.at>
2020-04-28 19:51:17 +0200

    simplify server setup by using grundstein/commons.lib.createServer

--- src/index.mjs
+++ src/index.mjs
@@ -5,42 +5,19 @@ import https from 'https'
 
 import { fs, log, middleware } from '@grundstein/commons'
 
+import { createServer } from '@grundstein/commons/lib.mjs'
+
 import proxy from './proxy.mjs'
 
 export const gps = async (config = {}) => {
   try {
-    const startTime = log.hrtime()
+    config.startTime = log.hrtime()
 
-    const { host = '0.0.0.0', port = 4343 } = config
-
-    const cwd = process.cwd()
-    const certDir = path.join(cwd, 'certificates')
-
-    let connector = http
-
-    const options = {}
-
-    const privCertFile = path.join(certDir, 'priv.pem')
-    const pubCertFile = path.join(certDir, 'pub.pem')
-
-    try {
-      options.key = await fs.readFile(privCertFile)
-      options.cert = await fs.readFile(pubCertFile)
-      connector = https
-    } catch(e) {
-       if (e.code !== 'ENOENT') {
-         throw e
-       }
-    }
-
-    const server = connector.createServer(options, proxy)
+    const server = await createServer(config, proxy(config))
 
     server.on('connection', socket => {
       socket.setNoDelay(true)
     })
-
-    const listener = middleware.listener({ host, port, startTime })
-    server.listen(port, host, listener)
   } catch (e) {
     log.error(e)
     process.exit(1)
